128d02c80f3721fc804f6910adec77c30948e9cc
saranyapat boonngam <saranyapa1234t@gmail.com>
2024-07-31 08:53:13 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Project_02',
+  webDir: 'www'
+};
+
+export default config;
